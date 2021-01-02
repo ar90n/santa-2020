ce0438bde838051d64745e74ff475f5bcc2a539b
Masahiro Wada <argon.argon.argon@gmail.com>
2021-01-02 11:18:03 +0900

    add comment and resource

--- src/santa_2020/agents/agent.py
+++ src/santa_2020/agents/agent.py
@@ -26,12 +26,32 @@ def try_to_str(agent: Agent) -> str:
     raise ValueError("failed to convert code to str.")
 
 
+def _comment_out(comment: str) -> str:
+    return "\n".join([f"# {l}" for l in comment.split("\n")])
+
+
 def try_to_submit_source(agent: Agent) -> ResultE[str]:
     from . import common
 
+    def _add_comment(source_lines: str, comment: Optional[str]) -> str:
+        if comment is None:
+            return source_lines
+        comment_lines = _comment_out(comment)
+        return f"{source_lines}\n\n{comment_lines}"
+
+    def _add_resource(source_lines: str, resource: Optional[dict[str, str]]) -> str:
+        if resource is None:
+            return source_lines
+        return f"{source_lines}\n\n_RESOURCE={str(resource)}"
+
     common_source_lines = inspect.getsource(common)
-    return try_to_str(agent).map(
-        lambda agent_source_lines: f"{common_source_lines}\n\n{agent_source_lines}"
+    return (
+        try_to_str(agent)
+        .map(
+            lambda agent_source_lines: f"{common_source_lines}\n\n{agent_source_lines}"
+        )
+        .map(lambda source_lines: _add_resource(source_lines, agent.resource))
+        .map(lambda source_lines: _add_comment(source_lines, agent.comment))
     )
 
 
@@ -43,7 +63,9 @@ def _init_agent_registry() -> tuple[Callable[[Agent], None], Callable[[str], Age
         _registry[key] = agent
 
     def _construct_agent(
-        key: str, resource: Optional[dict[Any, Any]] = None, comment: Optional[str] = None
+        key: str,
+        resource: Optional[dict[Any, Any]] = None,
+        comment: Optional[str] = None,
     ) -> Agent:
         nonlocal _registry
         return _registry[key](resource, comment)
--- src/santa_2020/agents/random.py
+++ src/santa_2020/agents/random.py
@@ -2,10 +2,13 @@ from .agent import Agent, register
 
 AGENT_KEY = "random"
 
+
 def agent(obs, conf):
     import random
 
     return random.randrange(conf.banditCount - 1)
 
 
-register(AGENT_KEY, lambda resource, comment: Agent(AGENT_KEY, agent, resource, comment))
+register(
+    AGENT_KEY, lambda resource, comment: Agent(AGENT_KEY, agent, resource, comment)
+)
